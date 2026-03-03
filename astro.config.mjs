1fe3661d8f1b83d615e42e3ffd6fb61a9f23801c
J Browning <jeremy.s.browning@gmail.com>
2026-03-03 15:35:59 -0500

    Added collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-
-    site: "https://dark-epics-blog-demo.netlify.app/"
+  site: "https://dark-epics-blog-demo.netlify.app/",
+  integrations: [preact()]
 });
